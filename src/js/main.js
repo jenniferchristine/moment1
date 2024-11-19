776048e1ac36581523f9bc16c1ad5ebe3acbf4b7
jenniferchristine <jeja2306@student.miun.se>
2024-11-19 12:34:17 +0100

    added textarea to form

--- src/js/main.js
+++ src/js/main.js
@@ -12,11 +12,14 @@ function addToForm(event) {
     const lastname = document.getElementById("lastname").value;
     const age = document.getElementById("age").value;
     const city = document.getElementById("city").value;
+    let other = document.getElementById("moreInfo").value;
 
     if (!surname || !lastname || !age || !city) {
         alert("Vänligen fyll i alla fält");
         return;
     }
+
+    other = other.trim() === "" ? "-" : other;
 
     const tBody = document.getElementById("tableBody");
     const newRow = document.createElement("tr");
@@ -27,6 +30,7 @@ function addToForm(event) {
     <td>${lastname}</td>
     <td>${age}</td>
     <td>${city}</td>
+    <td>${other}</td>
     `;
 
     tBody.appendChild(newRow);
